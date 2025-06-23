d43f785aa23458c12e1660f7698996de44311621
Diego <diego@teste.com>
2025-06-23 03:26:10 -0300

    logando request e response em dev

--- frontend/aluno-front/src/app/interceptors/log-http-interceptor.ts
+++ frontend/aluno-front/src/app/interceptors/log-http-interceptor.ts
@@ -1,11 +1,13 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
+  HttpResponse,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable()
@@ -16,11 +18,41 @@ export class LogHTTP implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     if (environment.logHttpRequests) {
+      console.log('🔵 REQUEST');
       console.log('URL:', req.urlWithParams);
       console.log('Método:', req.method);
-      console.log('Body:', req.body);
+      if (req.body) console.log('Body:', req.body);
       console.log('Headers:', req.headers);
+      console.log(
+        '==================================================================='
+      );
     }
-    return next.handle(req);
+    return next.handle(req).pipe(
+      tap({
+        next: (event: any) => {
+          if (event instanceof HttpResponse && environment.logHttpRequests) {
+            console.log('🟢 RESPONSE');
+            console.log('Status:', event.status);
+            console.log('URL:', event.url);
+            console.log('Body:', event.body);
+            console.log('========================');
+          }
+        },
+        error: (error: any) => {
+          if (environment.logHttpRequests) {
+            console.error('🔴 ERROR RESPONSE');
+            if (error instanceof HttpErrorResponse) {
+              console.error('Status:', error.status);
+              console.error('URL:', error.url);
+              console.error('Message:', error.message);
+              console.error('Error:', error.error);
+            } else {
+              console.error(error);
+            }
+            console.log('========================');
+          }
+        },
+      })
+    );
   }
 }
