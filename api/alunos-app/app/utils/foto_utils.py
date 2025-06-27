4989d9afe531c91f457c41d3c4d3dabc31b45ac7
Diego <diego@teste.com>
2025-06-27 02:16:48 -0300

    limpeza de código

--- api/alunos-app/app/utils/foto_utils.py
+++ api/alunos-app/app/utils/foto_utils.py
@@ -20,18 +20,6 @@ def salvar_foto(file):
     if not allowed_file(file.filename):
         return None
 
-    # os.makedirs(Config.UPLOAD_FOLDER, exist_ok=True)
-    # timestamp = int(time.time())
-    # filename_base = secure_filename(
-    #     file.filename.rsplit(".", 1)[0].lower().replace(" ", "_")
-    # )
-    # ext = os.path.splitext(file.filename)[1] or ".png"
-    # final_filename = f"{filename_base}_{timestamp}{ext}"
-    # foto_path = os.path.join(Config.UPLOAD_FOLDER, final_filename)
-
-    # image = Image.open(file.stream).convert("RGB")
-    # image.save(foto_path, format="JPEG", quality=50, optimize=True)
-
     # Gera nome único
     extensao = "jpg"  # você está forçando JPEG
 
