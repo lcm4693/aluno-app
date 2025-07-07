a7bb1fedffcbe35bca6d993b53d760402ead0361
Diego <diego@teste.com>
2025-07-07 15:33:39 -0300

    criação do item de menu cadastrar usuário

--- frontend/aluno-front/src/app/pages/menu/menu.component.ts
+++ frontend/aluno-front/src/app/pages/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
+import { UserStoreService } from '../../auth/services/user-store.service';
 
 @Component({
   selector: 'app-menu',
@@ -21,29 +22,44 @@ import { MenubarModule } from 'primeng/menubar';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {
-  items = [
-    {
-      label: 'Dashboard',
-      icon: 'pi pi-home',
-      routerLink: '/',
-    },
-    {
-      label: 'Alunos',
-      icon: 'pi pi-users',
-      routerLink: '/listar',
-    },
-    {
-      label: 'Calendário',
-      icon: 'pi pi-calendar',
-      routerLink: '/calendario-aulas',
-    },
-    // {
-    //   label: 'Notificações',
-    //   icon: 'pi pi-bell',
-    //   command: () => {
-    //     // abrir um overlayPanel ou navegação dedicada
-    //   },
-    // },
-  ];
+export class MenuComponent implements OnInit {
+  constructor(private userStorage: UserStoreService) {}
+  items: { label: string; icon: string; routerLink: string }[] | undefined =
+    undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-home',
+        routerLink: '/',
+      },
+      {
+        label: 'Alunos',
+        icon: 'pi pi-users',
+        routerLink: '/listar',
+      },
+      {
+        label: 'Calendário',
+        icon: 'pi pi-calendar',
+        routerLink: '/calendario-aulas',
+      },
+      // {
+      //   label: 'Notificações',
+      //   icon: 'pi pi-bell',
+      //   command: () => {
+      //     // abrir um overlayPanel ou navegação dedicada
+      //   },
+      // },
+    ];
+    const isAdmin = this.userStorage.getUsuario()?.isAdmin;
+
+    if (isAdmin) {
+      this.items.push({
+        label: 'Cadastrar usuário',
+        icon: 'pi pi-user-plus',
+        routerLink: '/cadastrar-usuario',
+      });
+    }
+  }
 }
