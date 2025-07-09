03d63b372f80632a800a9dc823bf9aa9cd0d483a
Diego <diego@teste.com>
2025-07-09 01:36:55 -0300

    criação da classe de miniatura de foto

--- frontend/aluno-front/src/app/pages/aulas/buscar-anotacoes-aulas/buscar-anotacoes-aulas.component.ts
+++ frontend/aluno-front/src/app/pages/aulas/buscar-anotacoes-aulas/buscar-anotacoes-aulas.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { debounceTime } from 'rxjs';
-import { Aula } from '../../../models/aula';
 import { InputTextModule } from 'primeng/inputtext';
-import { AlunoService } from '../../../services/aluno.service';
 import { AulaService } from '../../../services/aula.service';
 import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import { DatePickerModule } from 'primeng/datepicker';
+import { PanelModule } from 'primeng/panel';
+
+interface InterfaceAulaAnotacoes {
+  id: number;
+  dataAula: Date;
+  aluno_id: number;
+  nomeAluno: string;
+  anotacoes: string;
+  fotoUrl: string;
+}
 
 @Component({
   selector: 'app-buscar-anotacoes-aulas',
@@ -19,43 +29,28 @@ import { CardModule } from 'primeng/card';
     InputTextModule,
     ReactiveFormsModule,
     CardModule,
+    DialogModule,
+    DatePickerModule,
+    PanelModule,
+    FormsModule,
   ],
   templateUrl: './buscar-anotacoes-aulas.component.html',
   styleUrl: './buscar-anotacoes-aulas.component.css',
 })
 export class BuscarAnotacoesAulasComponent implements OnInit {
   searchControl = new FormControl('');
-  aulas: {
-    id: number;
-    dataAula: string;
-    aluno_id: number;
-    nomeAluno: string;
-    anotacoes: string;
-    fotoUrl: string;
-  }[] = [];
-  aulasFiltradas: {
-    id: number;
-    dataAula: string;
-    aluno_id: number;
-    nomeAluno: string;
-    anotacoes: string;
-    fotoUrl: string;
-  }[] = [];
+  hoje: Date = new Date();
+  aula: InterfaceAulaAnotacoes | undefined = undefined;
+  modalNovaDetalharAulaVisivel = false;
+  aulas: InterfaceAulaAnotacoes[] = [];
+  aulasFiltradas: InterfaceAulaAnotacoes[] = [];
 
   constructor(private aulaService: AulaService) {}
 
   ngOnInit(): void {
-    this.aulaService.listarAulas().subscribe(
-      (
-        aulas: {
-          id: number;
-          dataAula: string;
-          aluno_id: number;
-          nomeAluno: string;
-          anotacoes: string;
-          fotoUrl: string;
-        }[]
-      ) => {
+    this.aulaService
+      .listarAulas()
+      .subscribe((aulas: InterfaceAulaAnotacoes[]) => {
         this.aulas = aulas;
 
         this.searchControl.valueChanges
@@ -67,18 +62,20 @@ export class BuscarAnotacoesAulasComponent implements OnInit {
               return;
             }
             this.aulasFiltradas = this.aulas.filter((a) => {
-              // const textoAnotacao = this.removerAcentos(
-              //   Object.values(a).join(' ').toLowerCase()
-              // );
               const textoAnotacao = this.removerAcentos(a.anotacoes);
               return textoAnotacao.includes(t);
             });
           });
-      }
-    );
+      });
   }
 
   removerAcentos(texto: string): string {
     return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  selecionarAula(aula: InterfaceAulaAnotacoes) {
+    aula.dataAula = new Date(aula.dataAula);
+    this.aula = aula;
+    this.modalNovaDetalharAulaVisivel = true;
   }
 }
